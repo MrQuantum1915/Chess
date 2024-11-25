056a5736164f4682fef0485bf37c671773c31452
MrQuantum1915 <darshanpatelgdh@gmail.com>
2024-11-25 21:52:19 +0530

    update

--- main.c
+++ main.c
@@ -14,8 +14,13 @@
 #define whiteKnight 'N'
 #define whiteRook 'R'
 
+#define white 1 // Here I am maping int to defining whose move
+#define black 2
+
 void fillCanvas(char matrix[8][8]);
 void printCanvas(char matrix[8][8]);
+int mapCharToInt(char Column);
+void simulateMove(char piece, int InitialRow, int InitialColumn_int, int FinalRow, int FinalColumn_int);
 
 int main()
 {
@@ -32,8 +37,26 @@ int main()
   fillCanvas(matrix);
   printCanvas(matrix);
 
+  int whoseMove = white;
+
+  char InitialColumn;
+  int InitialRow;
+  char FinalColumn;
+  int FinalRow;
+
+  printf("Enter the Coordintes of block : ");
+  scanf("%c%d %c%d", &InitialColumn, &InitialRow, &FinalColumn, &FinalRow);
+  int InitialColumn_int = mapCharToInt(InitialColumn);
+  int FinalColumn_int = mapCharToInt(FinalColumn);
+
+  // printf("%d %d %d %d", InitialRow,InitialColumn_int,FinalRow,FinalColumn_int);
+
+  char piece = matrix[InitialRow][InitialColumn_int]; // To scan what the piece is at the position mentioned by user
+
+  simulateMove(piece, InitialRow, InitialColumn_int, FinalRow, FinalColumn_int);
+
   printf("\nPress any key to exit...");
-  getchar(); // If the user runs this in windows terminal instead, than user has a time to see the result
+  getchar(); // Added this If the user wanna runs this in windows terminal instead, than user has a time to see the result
   return 0;
 }
 
@@ -86,4 +109,64 @@ void fillCanvas(char matrix[8][8])
   matrix[7][2] = matrix[7][5] = whiteBishop;
   matrix[7][3] = whiteQueen;
   matrix[7][4] = whiteKing;
+}
+
+int mapCharToInt(char Column)
+{
+  switch (Column)
+  {
+  case 'a':
+    return 1;
+    break;
+
+  case 'b':
+    return 2;
+    break;
+
+  case 'c':
+    return 3;
+    break;
+
+  case 'd':
+    return 4;
+    break;
+
+  case 'e':
+    return 5;
+    break;
+
+  case 'f':
+    return 6;
+    break;
+
+  case 'g':
+    return 7;
+    break;
+
+  case 'h':
+    return 8;
+    break;
+  }
+}
+
+// void simulateMove(char piece, int InitialRow, int InitialColumn_int, int FinalRow, int FinalColumn_int) // I Implemented ANSI escape seqeunce to update specific part of canvas
+// {
+//   printf("\033[%d;%dH", (8-InitialRow), (InitialColumn_int+1));
+//   printf("%c",piece);
+//   fflush(stdout);
+// }
+
+void simulateMove(char piece, int InitialRow, int InitialColumn_int, int FinalRow, int FinalColumn_int)
+{
+  // Clear the previous position
+  printf("\033[%d;%dH", 10+8 - InitialRow, InitialColumn_int + 29);
+  
+  printf("\033[48;5;255m ");
+  getchar();
+  getchar();
+  // Move the piece to the new position
+  printf("\033[%d;%dH%c", 10+8 - FinalRow, FinalColumn_int + 29, piece);
+
+  // Reset cursor position to avoid overwriting
+  printf("\033[29;0H"); // Example reset (row 9, column 0)
 }
